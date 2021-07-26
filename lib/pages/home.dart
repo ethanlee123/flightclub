ae262883cca996520acfbb10725a1ae9d285d936
Ethan Lee <33e.lee@gmail.com>
2021-07-26 14:32:42 -0700

    setup google maps and displayed on home screen

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,19 +5,52 @@ import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-
 class Home extends StatefulWidget {
-  const Home({ Key? key }) : super(key: key);
+  const Home({Key? key}) : super(key: key);
 
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  static const _initialCameraPosition = CameraPosition(
+    target: LatLng(49.1876, -122.8583),
+    zoom: 11.5,
+  );
+
+  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(labelText: "search"),
+          ),
+          Container(
+            height: 300,
+            child: GoogleMap(
+                mapType: MapType.normal,
+                initialCameraPosition: _initialCameraPosition,
+                zoomControlsEnabled: false),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        currentIndex: 0,
+        // onTap: (index) => print(index)),
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+        ],
+      ),
     );
   }
 }
